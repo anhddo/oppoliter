55573e4fa86cb99f6f39bc4c1e6e6b0bd04915d2
anhddo <anhddo93@gmail.com>
2020-04-18 22:04:13 +0700

    update play

--- play.py
+++ play.py
@@ -12,26 +12,25 @@ import pandas as pd
 import seaborn as sns
 import sys
 from linear.avg_reward import Model, AverageReward, FourierTransform, FeatureTransformer, train
+import pickle
 
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
     parser = argparse.ArgumentParser(description="Finite-horizon MDP")
     parser.add_argument("--path")
-    parser.add_argument("--fourier-value", type=int, default=4)
+    parser.add_argument("--fourier-range", type=int, default=4)
     parser.add_argument("--n-step", type=int, default=10000)
     args = parser.parse_args()
     setting = vars(args)
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
-    #ftr_transform = FeatureTransformer(observation_space, n_components=100)
-    ftr_transform = FourierTransform(setting['fourier_value'], observation_space, env)
-    #print(ftr_transform.transform([1,-1,3,4]))
-    #import pdb; pdb.set_trace();
-    #import sys; sys.exit();
+    #ftr_transform = FourierTransform(setting['fourier_range'], observation_space, env)
     model = Model(0, 0)
-    model.load(setting['path'])
-    print(model.action_model[0].w)
+    model.load(path.join(setting['path'], 'model.pkl'))
+    ftr_transform = None
+    with open(path.join(setting['path'], 'ftr_transform'), 'rb') as f:
+        ftr_transform = pickle.load(f)
 
     episode_reward = 0
     terminal = True
