dcad873bbe4cc92f883f2ccd0ebfce65d45d7ade
anhddo <anhddo93@gmail.com>
2020-04-18 21:57:49 +0700

    torch code

--- linear_avg_reward.py
+++ linear_avg_reward.py
@@ -12,8 +12,9 @@ from datetime import datetime
 import pandas as pd
 import seaborn as sns
 import sys
-from linear.avg_reward import Model, AverageReward, FourierTransform, train
+from linear.avg_reward_torch import Model, Trajectory, AverageReward, FourierTransform, train
 import pickle
+import torch
 
 
 if __name__ == "__main__":
@@ -31,9 +32,16 @@ if __name__ == "__main__":
     setting = vars(args)
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
+    device = torch.device('cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
     #ftr_transform = FeatureTransformer(observation_space, n_components=setting['n_component'])
-    ftr_transform = FourierTransform(setting['fourier_value'], observation_space, env)
-    model = Model(ftr_transform.dimension, action_space)
+    ftr_transform = FourierTransform(setting['fourier_value'], observation_space, env, device)
+    model = Model(ftr_transform.dimension, action_space, device)
+
+    trajectory_per_action = [
+        Trajectory(ftr_transform.dimension, device) for _ in model.action_model
+    ]
     parent_dir = path.join('tmp', setting['save_dir_name'])
     os.makedirs(parent_dir, exist_ok=True)
     setting['model_path'] = path.join(parent_dir, 'model.pkl')
@@ -41,6 +49,6 @@ if __name__ == "__main__":
         pickle.dump(ftr_transform, f)
     with open(path.join(parent_dir, 'setting.txt'), 'w') as f:
         f.write(str(setting))
-    algo = AverageReward()
-    reward_track, time_step = train(env, algo, model, ftr_transform, setting)
+    algo = AverageReward(1e-3, ftr_transform.dimension, device)
+    reward_track, time_step = train(env, algo, model, ftr_transform, trajectory_per_action, setting)
     model.save(setting['model_path'])
